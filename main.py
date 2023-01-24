aa1e9316744cdc19e7491e20e64d4bba65af352d
shahz <hypergiant@gmail.com>
2023-01-24 17:01:12 -0600

    test submission

--- main.py
+++ main.py
@@ -60,8 +60,69 @@ def transform(raw_data_: pd.DataFrame, instruments_: pd.DataFrame) -> pd.DataFra
     :param raw_data_: dataframe of all features associated with instruments, with associated timestamps
     :param instruments_: dataframe of all traded instruments
     """
-    pass
+    tr = raw_data_.copy()
+    if 'Error' in raw_data_:
+        tr = tr.loc[tr['Error'] != 'Not Found']
+    tr['contract'] = tr['Term']
+    null_term = pd.isnull(tr['Term'])
+    tr.loc[null_term, 'contract'] = tr.loc[null_term, 'Period']
 
+    null_date = pd.isnull(tr['Trade Date'])
+    if any(null_date):
+        print('Warning: null dates found in raw data')
+        tr.drop(tr.index[null_date], inplace=True)
+
+    expired_instr = pd.to_datetime(tr['Expiration Date']) < pd.to_datetime(tr['Trade Date'])
+    if any(expired_instr):
+        print('Warning: expired instrument found in raw data')
+        tr.drop(tr.index[expired_instr], inplace=True)
+
+    null_contract = pd.isnull(tr['contract'])
+    if any(null_contract):
+        print('Warning: null contracts found in raw data')
+        tr.drop(tr.index[null_contract], inplace=True)
+
+    import re
+    tr['RIC'].apply((lambda x: re.search(r"\d", x[1:]).start()))
+    tr['base_ric'] = tr.apply(lambda x: x['RIC'].split(x['Volatility Surface Term'][:-1])[0], axis=1)
+    m_tr = tr.merge(instruments_,left_on='base_ric',right_on='Base')
+    m_tr['contract_year'] = pd.to_datetime(m_tr['Expiration Date']).dt.year
+    m_tr['contract_month'] = m_tr['Period'].str[0:3]
+    diff_year = m_tr['Expiration Date'].str[-1] != m_tr['Period'].str[-1]
+    m_tr.loc[diff_year, 'contract_year'] += 1
+
+    m_tr['month_code'] = m_tr['contract_month'].map(MONTH_NAME_TO_CODE)
+    m_tr.rename(columns=FIELDS_MAP, inplace=True)
+    m_tr['moneyness'] = m_tr['Volatility Surface Term'].str[:-1].astype(float)
+
+    m_tr['symbol'] = "FUTURE_VOL_"
+    bb_sym = m_tr['Bloomberg Ticker'].apply(lambda x: x if len(x) > 1 else x + '_')
+    m_tr['symbol'] = m_tr['symbol'] + m_tr['Exchange'] + '_' + bb_sym + m_tr['month_code'] + m_tr['contract_year'].astype(str) + '_' + m_tr['Volatility Surface Term'].str[:-1]
+
+    m_tr['source'] = 'refinitiv'
+    m_tr['date'] = pd.to_datetime(m_tr['date'])
+    for f in FLOAT_FIELDS:
+        if m_tr[f].dtype != float:
+            m_tr[f] = m_tr[f].str.replace(',', '').astype(float)
+    out_fields = ['date', 'symbol', 'source', 'contract_year', 'month_code', 'Base', 'moneyness']
+    out_fields.extend(FLOAT_FIELDS)
+
+    out = m_tr[out_fields]
+
+    out.set_index(['date','symbol','source','contract_year','month_code','Base','moneyness'],inplace=True)
+    out_s = pd.DataFrame(out.stack())
+
+    out_s.reset_index(inplace=True)
+    out_s.rename(columns={'level_7': 'field', 0: 'value'}, inplace=True)
+
+    out_s['field'] = pd.Categorical(out_s['field'], FLOAT_FIELDS)
+    out_s.sort_values(['field', 'Base', 'contract_year', 'month_code', 'moneyness'], inplace=True)
+    out_s.drop(['contract_year', 'Base', 'month_code', 'moneyness'], axis=1, inplace=True)
+    out_s['field'] = out_s['field'].astype(object)
+    out_s.reset_index(inplace=True)
+    out_s.drop('index', axis=1, inplace=True)
+
+    return out_s
 
 if __name__ == '__main__':
     raw_data = pd.read_csv("raw_data.csv")
